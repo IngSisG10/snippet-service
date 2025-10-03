aec8d6186dcf8643e340ef6093a125b9da162cb4
fersantisi <fer.santisi@hotmail.com>
2025-10-03 09:15:15 -0300

    spring setup

---
+++ src/test/kotlin/com/ingsis/grupo10/snippet/SnippetApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ingsis.grupo10.snippet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnippetApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
