0b1a497a9673d7f7f817dec2db1a6ea25744f28d
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2025-10-26 19:51:44 -0300

    feat: add asset service URL configuration for AssetClient

--- src/main/kotlin/com/ingsis/grupo10/snippet/config/AssetClientConfig.kt
+++ src/main/kotlin/com/ingsis/grupo10/snippet/config/AssetClientConfig.kt
@@ -7,7 +7,7 @@ import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
 class AssetClientConfig {
-    @Value("\${services.asset.base-url}")
+    @Value("\${asset.service.url}")
     private lateinit var assetServiceUrl: String
 
     @Bean
