39cce9d541dca52dcd167c7fd12177680744d4c8
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2025-11-23 21:22:42 -0300

    feat: update TestCreateRequest and TestResponseDto to require non-null input and output fields

--- src/main/kotlin/com/ingsis/grupo10/snippet/dto/TestCreateRequest.kt
+++ src/main/kotlin/com/ingsis/grupo10/snippet/dto/TestCreateRequest.kt
@@ -2,6 +2,6 @@ package com.ingsis.grupo10.snippet.dto
 
 data class TestCreateRequest(
     val name: String,
-    val input: List<String>?,
-    val output: List<String>?,
+    val input: List<String>,
+    val output: List<String>,
 )
