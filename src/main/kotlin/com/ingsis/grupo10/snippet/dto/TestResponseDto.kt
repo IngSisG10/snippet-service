009a72613f2366ddb4b8d2ff90f9fd45d5750744
carlix-27 <128094132+carlix-27@users.noreply.github.com>
2025-11-23 21:35:17 -0300

    resolve bad request. Including emptyList() in TestResponse and TestCreateRequest

--- src/main/kotlin/com/ingsis/grupo10/snippet/dto/TestResponseDto.kt
+++ src/main/kotlin/com/ingsis/grupo10/snippet/dto/TestResponseDto.kt
@@ -5,6 +5,6 @@ import java.util.UUID
 data class TestResponseDto(
     val id: UUID,
     val name: String,
-    val input: List<String>,
-    val output: List<String>,
+    val input: List<String> = emptyList(),
+    val output: List<String> = emptyList(),
 )
